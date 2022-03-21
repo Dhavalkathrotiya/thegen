b626d4b8181c50a960bd61ad94004ba023d92473
Dhaval rk dk <pdhaval988@gmail.com>
2022-03-21 20:59:11 +0000

    thegen

--- src/app.js
+++ src/app.js
@@ -5,7 +5,20 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let pronoun = ["the", "our"];
+const adj = ["great", "big"];
+const noun = ["jogger", "racoon"];
+
+(function(x, y, z) {
+  let domainName = [];
+  for (var i = 0, l = x.length; i < l; i++) {
+    for (var j = 0, k = 0; j < y.length; j++) {
+      domainName.push([pronoun[i], adj[j], noun[k]] + ".com");
+      if (j == y.length - 1 && k < z.length - 1) {
+        k++;
+        j = -1;
+      }
+    }
+  }
+  console.log(domainName.join("\n").replaceAll(",", ""));
+})(pronoun, adj, noun);
